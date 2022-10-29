0f5e48d62a401781aa576ef46ab5727f8a6a5da6
neronix17 <39186138+neronix17@users.noreply.github.com>
2022-10-29 06:19:30 +0100

    Fixed Infinite Egg Laying
    When a saurid and human did the horizontal monster mash you would for some still unknown reason you would get infinite eggs spawning and error spam.
    
    As far as I can tell that seems to be fixed!

--- 1.4/Source/VRESaurids/Patch_Hediff_Pregnant_Tick.cs
+++ 1.4/Source/VRESaurids/Patch_Hediff_Pregnant_Tick.cs
@@ -27,7 +27,7 @@ namespace VRESaurids
 					Hediff hediff = HediffMaker.MakeHediff(VRESauridsDefOf.VRESaurids_EggFatigue, __instance.pawn);
 					__instance.pawn.health.AddHediff(hediff);
 					// Tell the player.
-					LetterMaker.MakeLetter("VRESaurids.EggLaidLabel".Translate(__instance.mother.LabelShort), "VRESaurids.EggLaidDesc".Translate(__instance.mother.LabelShort), LetterDefOf.PositiveEvent, new LookTargets(egg));
+					LetterMaker.MakeLetter("VRESaurids.EggLaidLabel".Translate(__instance.pawn.LabelShort), "VRESaurids.EggLaidDesc".Translate(__instance.pawn.LabelShort), LetterDefOf.PositiveEvent);
 					__instance.pawn.health.RemoveHediff(__instance);
 				}
 				catch (Exception ex)
